01ad8918a2646655398ca02454b5140b6f7f943c
Ashwin-IAST <ashwin.krishna@iast-software.com>
2025-02-05 12:27:28 +0530

    Create transmission_control.h

---
+++ include/transmission_control.h
@@ -0,0 +1,9 @@
+#ifndef TRANSMISSION_CONTROL_H
+#define TRANSMISSION_CONTROL_H
+
+#include "common.h"
+
+void transmission_control_start(void);
+void transmission_control_update(void);
+
+#endif // TRANSMISSION_CONTROL_H
