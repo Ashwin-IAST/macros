452a1c98c7991cd53a3e9ee737426a5b27f492f0
Ashwin-IAST <ashwin.krishna@iast-software.com>
2025-02-05 12:23:52 +0530

    Create project_config.h

---
+++ config/project_config.h
@@ -0,0 +1,11 @@
+#ifndef PROJECT_CONFIG_H
+#define PROJECT_CONFIG_H
+
+// General system macros
+#define DEBUG_MODE       1
+#define MAX_RPM          7000
+#define MIN_RPM          800
+#define TRANSMISSION_GEAR_COUNT 6
+#define ENABLE_DIAGNOSTICS 1
+
+#endif // PROJECT_CONFIG_H
